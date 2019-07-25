3dfc0baf3309a7596fbec87166d6821e8ba73042
pedronalvarez <pedronalvarez@hotmail.com>
2019-07-24 23:32:08 -0300

    created mocks for testing

--- MarvelChallengeTests/Character List/CharacterListInteractorMock.swift
+++ MarvelChallengeTests/Character List/CharacterListInteractorMock.swift
@@ -11,13 +11,15 @@ import Foundation
 
 final class CharacterListManagerMock: CharacterListManagerInput{
     
-    let characterList: [CharacterEntity] = [CharacterEntity(]
+    let characterList: [CharacterEntity] = [CharacterEntity(id: 0, name: "Test0", thumb: "Thumb0", charDescription: "Description0", comicList: []), CharacterEntity(id: 1, name: "Test1", thumb: "Thumb1", charDescription: "Description1", comicList: []), CharacterEntity(id: 2, name: "Test2", thumb: "Thumb2", charDescription: "Description2", comicList: []), CharacterEntity(id: 3, name: "Test3", thumb: "Thumb3", charDescription: "Description3", comicList: [])]
     
     func getInitialCharacterList(completion: @escaping ([CharacterEntity]) -> ()) {
         
+        completion(characterList)
     }
     
     func getNewCharacter(offset: Int, completion: @escaping (CharacterEntity) -> ()) {
-        <#code#>
+        
+        completion(characterList[offset])
     }
 }
--- MarvelChallengeTests/Character List/CharacterListManagerMock.swift
+++ MarvelChallengeTests/Character List/CharacterListManagerMock.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+@testable import MarvelChallenge
+
+final class CharacterListInteractorMock: CharacterListInteractorInput{
+    
+    weak var output: CharacterListInteractorOutput?
+    
+    let characterList: [CharacterEntity] = [CharacterEntity(id: 0, name: "Test0", thumb: "Thumb0", charDescription: "Description0", comicList: []), CharacterEntity(id: 1, name: "Test1", thumb: "Thumb1", charDescription: "Description1", comicList: []), CharacterEntity(id: 2, name: "Test2", thumb: "Thumb2", charDescription: "Description2", comicList: []), CharacterEntity(id: 3, name: "Test3", thumb: "Thumb3", charDescription: "Description3", comicList: [])]
+    
+    func firstLoad() {
+        
+        output?.loadedCharacters(characters: characterList)
+    }
+    
+    func loadNextCharacter(offset: Int) {
+        
+        output?.loadedCharacters(characters: [characterList[0]])
+    }
+}
